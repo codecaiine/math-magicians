764ac5ab7401f99e63a1c4a84aefbea51b6f812d
codecaiine <akabrouyannickn@gmail.com>
2021-09-21 11:56:19 +0000

    Set up linters config

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        h1 > I am Yannick Noel Aka < /h1> <
+        p > I 'm learning React at Microverse</p> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
