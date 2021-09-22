a52bf00df2da66e79d6b97fba18e9c3b1b043007
codecaiine <akabrouyannickn@gmail.com>
2021-09-22 13:50:37 +0000

    Add react structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          I am Yannick Noel Aka
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-        </h1>
-
-        <p>
-          I am learning React at Microverse
-          {' '}
-        </p>
-
-      </header>
-    </div>
-  );
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            Calculator / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
