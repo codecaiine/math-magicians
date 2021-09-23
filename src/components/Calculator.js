ecbbb7f18ff951185cdf6c6da8b8391dc22bc03b
codecaiine <akabrouyannickn@gmail.com>
2021-09-23 21:35:51 +0000

    Remove redundant import

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,122 +1,107 @@
-import React from 'react';
+import { useCallback, useState } from 'react';
 import calculate from '../logic/calculate';
 
 const buttons = [{
-  text: 'AC',
-},
-{
-  text: '+/-',
-},
-{
-  text: '%',
-},
-{
-  text: '÷',
-  className: 'bg-orange',
-},
-{
-  text: '7',
-},
-{
-  text: '8',
-},
-{
-  text: '9',
-},
-{
-  text: 'x',
-  className: 'bg-orange',
-},
-{
-  text: '4',
-},
-{
-  text: '5',
-},
-{
-  text: '6',
-},
-{
-  text: '-',
-  className: 'bg-orange',
-},
-{
-  text: '1',
-},
-{
-  text: '2',
-},
-{
-  text: '3',
-},
-{
-  text: '+',
-  className: 'bg-orange',
-},
-{
-  text: '0',
-  className: 'span-2-col',
-},
-{
-  text: '.',
-},
-{
-  text: '=',
-  className: 'bg-orange',
-},
+        text: 'AC',
+    },
+    {
+        text: '+/-',
+    },
+    {
+        text: '%',
+    },
+    {
+        text: '÷',
+        className: 'bg-orange',
+    },
+    {
+        text: '7',
+    },
+    {
+        text: '8',
+    },
+    {
+        text: '9',
+    },
+    {
+        text: 'x',
+        className: 'bg-orange',
+    },
+    {
+        text: '4',
+    },
+    {
+        text: '5',
+    },
+    {
+        text: '6',
+    },
+    {
+        text: '-',
+        className: 'bg-orange',
+    },
+    {
+        text: '1',
+    },
+    {
+        text: '2',
+    },
+    {
+        text: '3',
+    },
+    {
+        text: '+',
+        className: 'bg-orange',
+    },
+    {
+        text: '0',
+        className: 'span-2-col',
+    },
+    {
+        text: '.',
+    },
+    {
+        text: '=',
+        className: 'bg-orange',
+    },
 ];
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+    const [state, setState] = useState({});
 
-    this.state = {};
-  }
+    const onClickHandler = useCallback((event) => {
+        setState((prev) => {
+            const output = calculate(prev, event.target.value);
+            return ({
+                ...prev,
+                ...output,
+            });
+        });
+    }, []);
 
-    onClickHandler = (event) => {
-      this.setState((prevState) => calculate(prevState, event.target.value));
-    };
+    const { total, next, operation } = state;
+    const display = (total || '') + (operation || '') + (next || '');
 
-    formatOutput = () => {
-      const { total, next, operation } = this.state;
-      const display = (total || '') + (operation || '') + (next || '');
-
-      return display || '0';
-    };
-
-    render() {
-      return (
-        <div className="calculator">
-          <div className="output">
-            <p>
-              {' '}
-              { this.formatOutput() }
-              {' '}
-
-            </p>
-          </div>
-          {' '}
-          <div className="grid">
-            {' '}
-            {
-                buttons.map(({ text, className }) => (
-                  <input
-                    value={text}
-                    className={`button grid-item ${className || ''}`}
-                    key={text}
-                    type="button"
-                    onClick={this.onClickHandler}
-                  />
-                ))
-            }
-            {' '}
-
-          </div>
-          {' '}
-
-        </div>
-      );
-    }
-}
+    return ( <
+        div className = "calculator" >
+        <
+        div className = "output" >
+        <
+        p > { display || '0' } < /p> <
+        /div> <
+        div className = "grid" > {
+            buttons.map(({ text, className }) => ( <
+                input value = { text }
+                className = { `button grid-item ${className || ''}` }
+                key = { text }
+                type = "button"
+                onClick = { onClickHandler }
+                />
+            ))
+        } <
+        /div> <
+        /div>
+    );
+};
 
 export default Calculator;
