ddcc3cdf4bb3a6f20fc7f155a2819b70bba3cb74
codecaiine <akabrouyannickn@gmail.com>
2021-09-28 13:53:58 +0000

    Add routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NotMatch from './pages/NotMatch';
 
 const App = () => (
   <Router basename={process.env.PUBLIC_URL}>
@@ -10,6 +13,22 @@ const App = () => (
         <Route path="/calculator">
           <Calculator />
         </Route>
+        {' '}
+        <Route path="/quote">
+          <Quote />
+        </Route>
+        {' '}
+        <Route
+          exact
+          path="/"
+        >
+          <Home />
+        </Route>
+        {' '}
+        <Route path="*">
+          <NotMatch />
+        </Route>
+
       </Switch>
     </div>
   </Router>
