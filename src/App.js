fef09bf0d3e48e18aa9e3797aebdfd9fdc95cc91
codecaiine <akabrouyannickn@gmail.com>
2021-09-28 13:06:04 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './pages/Navbar';
 
-const App = () => <Calculator />;
+const App = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <div className="app">
+      <Navbar />
+      <Switch>
+        <Route path="/calculator">
+          <Calculator />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
