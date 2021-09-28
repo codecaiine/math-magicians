2a89cdb1318972c6200721c419a849eb2c1eb9df
codecaiine <akabrouyannickn@gmail.com>
2021-09-28 13:47:35 +0000

    Add home Quote and NotMatch pages

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+const Home = () => (
+  <div>
+    <div className="homePageWrap">
+      <h1>
+        Welcome to our page
+        {' '}
+      </h1>
+      {' '}
+      <p className="homeParagraph">
+        Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.Fusce tellus augue,
+        tincidunt malesuada nulla sed,
+        rutrum porttitor dui..
+        {' '}
+      </p>
+      {' '}
+      <p className="homeParagraph">
+        Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.Fusce tellus augue,
+        tincidunt malesuada nulla sed,
+        rutrum porttitor dui.
+        {' '}
+      </p>
+
+    </div>
+  </div>
+);
+
+export default Home;
---
+++ src/pages/Quote.js
@@ -0,0 +1,14 @@
+const Quote = () => (
+  <p className="quoteParagraph">
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    It is about understanding.
+    {' '}
+    <b>
+      {' '}
+      -William Paul Thurston
+      {' '}
+    </b>
+  </p>
+);
+
+export default Quote;
